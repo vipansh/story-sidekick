2b7de2ffc6c1d859e96d61d622f30c601d7bb825
vipansh <vipanshthakur@gmail.com>
2024-02-07 02:31:53 +0530

    feat: remove pin container

--- @/components/AllBlogsCard.tsx
+++ @/components/AllBlogsCard.tsx
@@ -4,7 +4,6 @@ import Link from "next/link";
 import { getAllBlogs } from "../lib/supabase";
 import { formetDate } from "../lib/utils";
 import { AspectRatio } from "./ui/aspect-ratio";
-import { PinContainer } from "./ui/3d-pin";
 import { LayoutComponent } from "./ui/layout-comp";
 
 type Props = {};
@@ -57,34 +56,28 @@ const AllBlogsCard = ({}: Props) => {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 max-w-7xl mx-auto">
       {data.map((blog) => (
         <Link href={`/blog/${blog.id}`} passHref key={blog.id}>
-          <PinContainer
-            title={blog.title}
-            href={`/blog/${blog.id}`}
-            className="row-span-1 rounded-xl group/bento bg-white border border-transparent space-y-4"
-          >
-            <div className="flex flex-col p-4 tracking-tight text-slate-100/50 w-full h-full">
-              <h3 className="!pb-2 !m-0 font-bold text-base text-slate-800">
-                {blog.title}
-              </h3>
-              <div className="text-base !m-0 !p-0 font-normal">
-                <span className="text-slate-500">
-                  {formetDate(blog.created_at)}
-                </span>
-              </div>
-              <div className="flex flex-1 mt-4 rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500">
-                <AspectRatio ratio={1} className="relative w-full h-full">
-                  <LayoutComponent layoutId={blog.title}>
-                    <img
-                      alt={blog.title}
-                      src={blog.imageUrl}
-                      className="rounded-lg w-full h-full object-cover"
-                      loading="lazy"
-                    />
-                  </LayoutComponent>
-                </AspectRatio>
-              </div>
+          <div className=" row-span-1 rounded-xl group/bento bg-white border border-transparent space-y-4 flex flex-col p-4 tracking-tight text-slate-100/50 w-full h-full">
+            <h3 className="!pb-2 !m-0 font-bold text-base text-slate-800">
+              {blog.title}
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500">
+                {formetDate(blog.created_at)}
+              </span>
             </div>
-          </PinContainer>
+            <div className="flex flex-1 mt-4 rounded-lg bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500">
+              <AspectRatio ratio={1} className="relative w-full h-full">
+                <LayoutComponent layoutId={blog.title}>
+                  <img
+                    alt={blog.title}
+                    src={blog.imageUrl}
+                    className="rounded-lg w-full h-full object-cover"
+                    loading="lazy"
+                  />
+                </LayoutComponent>
+              </AspectRatio>
+            </div>
+          </div>
         </Link>
       ))}
     </div>
