2f0db9174891fa5d6fe8384762e09b2dff51a875
vipansh <vipanshthakur@gmail.com>
2024-02-07 01:31:38 +0530

    ✅ feat: tracing beam removed and tool tip added

--- @/components/BlogCard.tsx
+++ @/components/BlogCard.tsx
@@ -1,17 +1,14 @@
 import React, { ReactNode } from "react";
 import { Card } from "./ui/card";
-import { TracingBeam } from "./ui/tracing-beam";
 
 type BlogCardProps = {
   children: ReactNode;
 };
 
 const BlogCard: React.FC<BlogCardProps> = ({ children }) => (
-  <TracingBeam>
-    <Card className="max-w-2xl mx-auto antialiased pt-4 relative my-4 pb-16  lg:max-w-4xl p-6 md:p-8 py-12 bg-white shadow-lg rounded-lg">
-      {children}
-    </Card>
-  </TracingBeam>
+  <Card className="max-w-2xl mx-auto antialiased pt-4 relative my-4 pb-16  lg:max-w-4xl p-6 md:p-8 py-12 bg-white shadow-lg rounded-lg">
+    {children}
+  </Card>
 );
 
 export default BlogCard;
--- @/components/BlogPage.tsx
+++ @/components/BlogPage.tsx
@@ -17,28 +17,32 @@ const BlogPage: React.FC = () => {
   return (
     <BlogCard>
       <BlogHeader />
+      <div className=" mx-auto antialiased pt-4 relative">
+        <h3 className="mt-5 mb-3 text-2xl font-semibold text-gray-800">
+          {blogData.content.title}
+        </h3>
 
-      <h3 className="mt-5 mb-3 text-2xl font-semibold text-gray-800">
-        {blogData.content.title}
-      </h3>
+        <BlogImage
+          imageUrl={blogData.imageUrl}
+          title={blogData.content.title}
+        />
 
-      <BlogImage imageUrl={blogData.imageUrl} title={blogData.content.title} />
+        <article className="prose lg:prose-lg max-w-none">
+          {blogData?.content?.content?.map((section, index) => (
+            <div key={index}>
+              {section.data.map((item, itemIndex) => (
+                <React.Fragment key={itemIndex}>
+                  <CardContent>
+                    <Markdown>{item}</Markdown>
+                  </CardContent>
 
-      <article className="prose lg:prose-lg max-w-none">
-        {blogData?.content?.content?.map((section, index) => (
-          <div key={index}>
-            {section.data.map((item, itemIndex) => (
-              <React.Fragment key={itemIndex}>
-                <CardContent>
-                  <Markdown>{item}</Markdown>
-                </CardContent>
-
-                <Separator className="my-1" />
-              </React.Fragment>
-            ))}
-          </div>
-        ))}
-      </article>
+                  <Separator className="my-1" />
+                </React.Fragment>
+              ))}
+            </div>
+          ))}
+        </article>
+      </div>
     </BlogCard>
   );
 };
