30e844b1b02faa7014884514aab139d0e47597ac
vipansh <vipanshthakur@gmail.com>
2024-02-09 01:43:24 +0530

    🐞fix: toster not showing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { Toaster } from "sonner";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Toaster />
+      <Component {...pageProps} />
+    </>
+  );
 }
-
-export default MyApp
+export default MyApp;
