daa66fd2bbffa212fabb447f290e578823c39c06
vipansh <vipanshthakur@gmail.com>
2024-02-10 19:54:03 +0530

    🚀 fix: change from getStaticPaths to getServerSideprop

--- pages/blog/[id]/index.tsx
+++ pages/blog/[id]/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BlogPage from "../../../@/components/BlogPage";
-import { getAllBlogs, getBlogById } from "../../../@/lib/supabase";
+import { getBlogById } from "../../../@/lib/supabase";
 
 const index = ({ blogData }) => {
   return <BlogPage blogData={blogData} />;
@@ -8,23 +8,15 @@ const index = ({ blogData }) => {
 
 export default index;
 
-export const getStaticPaths = async () => {
-  const blogs = await getAllBlogs();
-  return {
-    paths: blogs.data.map((blog) => ({
-      params: {
-        id: blog.id.toString(),
-      },
-    })),
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async ({ params }) => {
-  const id = params.id;
+export const getServerSideProps = async ({ query }) => {
+  const id = query.id;
   const { data } = await getBlogById(Number(id));
-  return { props: { blogData:{
-    content: JSON.parse(data?.content || `{}`),
-    imageUrl: data?.imageUrl,
-  }} };
+  return {
+    props: {
+      blogData: {
+        content: JSON.parse(data?.content || `{}`),
+        imageUrl: data?.imageUrl,
+      },
+    },
+  };
 };
