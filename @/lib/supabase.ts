ac8bed23263621ba3d4a2860569a27a5d9c178f4
vipansh <vipanshthakur@gmail.com>
2024-02-12 02:23:42 +0530

    🐞 fix: NEXT_PUBLIC_SUPABASE_ANON_KEY typo in supabase

--- @/lib/supabase.ts
+++ @/lib/supabase.ts
@@ -1,5 +1,5 @@
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_key;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 import { createClient } from "@supabase/supabase-js";
 
