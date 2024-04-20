3e446bed5d5789406561accede182f9d9b78a1cf
Vipansh <vipanshthakur@gmail.com>
2024-04-20 20:44:15 +0530

    ✅feat: posthog added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
 import { AppProps } from "next/app";
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 import React from "react";
 import { Toaster } from "sonner";
 
@@ -10,12 +12,27 @@ global.performance = global.performance || {
   now: () => new Date().getTime(),
 };
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+if (typeof window !== "undefined") {
+  // checks that we are client-side
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug(); // debug mode in development
+    },
+  });
+}
+
+const MyApp: React.FC<AppProps> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-      <Toaster />
-    </UserProvider>
+    <PostHogProvider client={posthog}>
+      <UserProvider>
+        <Component {...pageProps} />
+        <Toaster />
+      </UserProvider>
+    </PostHogProvider>
   );
 };
 
