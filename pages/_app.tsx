bf5babf58ab391e5b3d5f1c887940ea46c5ba246
Vipansh <vipanshthakur@gmail.com>
2024-04-24 00:29:40 +0530

    ✅ feat: POSTHOG_KEY added in env example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,7 +17,7 @@ if (typeof window !== "undefined") {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
     loaded: (posthog) => {
-      if (process.env.NODE_ENV !== "development") posthog.debug(); // debug mode in development
+      posthog.debug(); // debug mode in development
     },
   });
 }
