b976d7c05f2ef01b043e33f5a3e287cd9e0369ca
Vipansh <vipanshthakur@gmail.com>
2024-04-24 03:10:51 +0530

    ✅ feat: messaeg change for loading

--- @/components/optionsList/HeadingSelectionModal.tsx
+++ @/components/optionsList/HeadingSelectionModal.tsx
@@ -59,7 +59,9 @@ const HeadingSelectionModal: React.FC<Props> = ({
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="py-4 w-5/6 mx-auto">
-        {isGeneratingPost && <Loader message="Generating blog post..." />}
+        {isGeneratingPost && (
+          <Loader message="Generating blog post... This usually takes about 30 seconds." />
+        )}
         <HeadingSelectionComponent
           options={chosenHeadings}
           handleOptionSelection={handleOptionSelection}
